6ce414d533ddbada781031a4074d7c9efcb7222e
Endriu12 <github2017>
2017-07-04 19:26:50 +0300

    To add some commit for understanding

--- src/com/perepelitsya/Solution.java
+++ src/com/perepelitsya/Solution.java
@@ -8,12 +8,21 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int height = 1;
+        int height = 1; //it's height of Utopian Tree
         int n = in.nextInt();
-        if (n <= 10 && n >= 1) {
+        if (n <= 10 && n >= 1) { //Restrictions on n
             for (int a0 = 0; a0 < n; a0++) {
                 int cycle = in.nextInt();
-                if (cycle <= 60 && cycle >= 0) {
+                if (cycle <= 60 && cycle >= 0) { //Restrictions on cycles
+                    //It's our main logic
+                    /*
+                    if n =  0, we don't change the height
+                    if n = another number. For example 3
+                    n=3;  we use cycle j = 0, j = 1, j = 2
+                    1. 0 % 2 == 0   false,  so height += 1;    height = 2
+                    2. 1 % 2 == 0   false,  so height += 1;    height = 3
+                       2 % 2 == 0   true,   so height += height;    height =6.
+                     */
                     if (cycle == 0) {
                         height = 1;
                     } else {
@@ -25,8 +34,8 @@ public class Solution {
                             }
                         }
                     }
-                    System.out.println(height);
-                    height = 1;
+                    System.out.println(height);//it's our result
+                    height = 1; //after all we change height of tree to his start value
                 } else {
                     System.out.println("N must be 0<=N<=60");
                 }
